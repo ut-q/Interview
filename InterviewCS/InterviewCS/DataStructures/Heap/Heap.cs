dfeb69dff20a6f6405dd09126288bdc19911db00
Utku Akay <utku.akay@gmail.com>
2019-11-29 12:53:42 -0800

    Heap

---
+++ InterviewCS/InterviewCS/DataStructures/Heap/Heap.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using InterviewCS.DataStructures.Vector;
+
+namespace InterviewCS.DataStructures.Heap
+{
+    //TODO make this for heaps that are not min heap as well
+
+    public class Heap<T> : IEnumerable<T> where T: IComparable<T>
+    {
+        #region Properties and Constants
+
+        public int Count => Data.Count;
+
+        #endregion
+
+        #region Constructors and Operators
+
+        public Heap()
+        {
+            Data = new Vector<T>();
+        }
+
+        public Heap(IEnumerable<T> list)
+        {
+            Data = new Vector<T>(list);
+
+            for (var i = Count - 1; i >= 0; --i)
+            {
+                BubbleDown(i);
+            }
+        }
+
+        public Heap(int capacity)
+        {
+            Data = new Vector<T>(capacity);
+        }
+
+        #endregion
+
+        #region Functions
+
+        public void Clear()
+        {
+            Data.Clear();
+        }
+
+        public T ExtractFirst()
+        {
+            return RemoveAt(0);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public Vector<T> HeapSort()
+        {
+            var vector = new Vector<T>();
+            var n = Count;
+
+            for (var i = 0; i < n; ++i)
+            {
+                vector.Add(ExtractFirst());
+            }
+
+            return vector;
+        }
+
+        public void Insert(T item)
+        {
+            Data.Add(item);
+
+            BubbleUp(Count - 1);
+        }
+
+        public T RemoveAt(int index)
+        {
+            if (!IsValidIndex(index))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var item = Data[index];
+
+            Data.Swap(index, Count - 1);
+
+            Data.RemoveAt(Count - 1);
+
+            BubbleDown(index);
+
+            return item;
+        }
+
+        public void TrimExcess()
+        {
+            Data.TrimExcess();
+        }
+
+        public T[] ToArray()
+        {
+            return Data.ToArray();
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+
+        #endregion
+
+        #region Private Members
+
+        private Vector<T> Data { get; set; }
+        private const int RootParent = -1;
+
+        private static int ParentIndex(int index)
+        {
+            if (index == 0)
+            {
+                return RootParent;
+            }
+            return (index - 1) / 2;
+        }
+
+        private static int LeftChildIndex(int index)
+        {
+            return 2 * index + 1;
+        }
+
+        private static int RightChildIndex(int index)
+        {
+            return 2 * index + 2;
+        }
+
+        private void BubbleDown(int index)
+        {
+            //find the smallest of the parent and the children
+
+            var minIndex = index;
+
+            if (IsValidIndex(LeftChildIndex(index)))
+            {
+                minIndex = Data[minIndex].CompareTo(Data[LeftChildIndex(index)]) < 0 ? minIndex : LeftChildIndex(index);
+            }
+
+            if (IsValidIndex(RightChildIndex(index)))
+            {
+                minIndex = Data[minIndex].CompareTo(Data[RightChildIndex(index)]) < 0 ? minIndex : RightChildIndex(index);
+            }
+
+            if (minIndex != index)
+            {
+                Data.Swap(minIndex, index);
+                BubbleDown(minIndex);
+            }
+        }
+
+        private void BubbleUp(int index)
+        {
+            if (IsRoot(index))
+            {
+                return;
+            }
+
+            if (Data[ParentIndex(index)].CompareTo(Data[index]) > 0)
+            {
+                Data.Swap(ParentIndex(index), index);
+
+                BubbleUp(ParentIndex(index));
+            }
+        }
+
+        private static bool IsRoot(int index)
+        {
+            return ParentIndex(index) == RootParent;
+        }
+
+        private bool IsValidIndex(int index)
+        {
+            return index >= 0 && index < Count;
+        }
+
+        #endregion
+
+    }
+}
---
+++ InterviewCS/InterviewCS/UnitTests/DataStructures/Test_Heap.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterviewCS.DataStructures.Heap;
+
+namespace InterviewCS.UnitTests.DataStructures
+{
+    public class Test_Heap : ITestable
+    {
+
+        public void Test()
+        {
+            var h1 = new Heap<int>();
+
+            h1.Insert(6);
+            h1.Insert(7);
+            h1.Insert(11);
+            h1.Insert(5);
+            h1.Insert(6);
+            h1.Insert(9);
+            h1.Insert(1);
+            h1.Insert(3);
+            h1.Insert(4);
+
+            Console.WriteLine(h1);
+
+            h1.RemoveAt(4);
+
+            Console.WriteLine(h1);
+
+            Console.WriteLine("Sorted heap: " + h1.HeapSort());
+
+            Console.WriteLine("Heap after: " + h1);
+
+            var h2 = new Heap<int>(new int[]{5,3,2,7,8,9,3,6,1,8});
+
+            Console.WriteLine(h2);
+            Console.WriteLine(h2.HeapSort());
+
+            var h3 = new Heap<string>(new string[]{"a","b","c","abcde","h","y","dsffdsfds", " test"});
+
+            Console.WriteLine(h3);
+            Console.WriteLine(h3.HeapSort());
+        }
+    }
+}
